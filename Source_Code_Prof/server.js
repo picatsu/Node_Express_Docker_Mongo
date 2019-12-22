8ae602afb4dfa65b3e0a23f4fabd3f2aa24a04bf
picatsu <achraf.pica@gmail.com>
2019-12-22 21:42:28 +0100

    fix css issue

--- Source_Code_Prof/server.js
+++ Source_Code_Prof/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const bodyParser = require ('body-parser');
+const bodyParser = require('body-parser');
 
 
 
@@ -9,7 +9,7 @@ const server = require('http').createServer(app);
 const io = require('socket.io').listen(server);
 const PORT = 3000,
     request = require('request'),
-    fetch   = require('node-fetch');
+    fetch = require('node-fetch');
 server.listen(PORT);
 console.log('Server is running');
 const rp = require('request-promise');
@@ -17,19 +17,19 @@ const rp = require('request-promise');
 
 const connections = [];
 let questionMap = new Map();
-questionMap.set(0, ' Donne ton birthname' );
-questionMap.set(1, ' Donne ton lastname' );
-questionMap.set(2, ' Donne ton SSN' );
+questionMap.set(0, ' Donne ton birthname');
+questionMap.set(1, ' Donne ton lastname');
+questionMap.set(2, ' Donne ton SSN');
 let cpt = 0;
 
 let dataMap = new Map();
-let serverResponse ='';
+let serverResponse = '';
 
 
-io.sockets.on('connection',(socket) => {
+io.sockets.on('connection', (socket) => {
     connections.push(socket);
     console.log(' %s sockets is connected', connections.length);
-    io.sockets.emit('new message', {message: questionMap.get(cpt)});
+    io.sockets.emit('new message', { message: questionMap.get(cpt) });
 
 
     socket.on('disconnect', () => {
@@ -38,31 +38,30 @@ io.sockets.on('connection',(socket) => {
 
     socket.on('sending message', (message) => {
         console.log('Message is received :', message);
-        io.sockets.emit('new message', {message: ' ==> you said : '+message});
+        io.sockets.emit('new message', { message: ' ==> you said : ' + message });
 
         cpt++;
-        if(cpt != 3 ){
-            io.sockets.emit('new message', {message: questionMap.get(cpt)});
+        if (cpt != 3) {
+            io.sockets.emit('new message', { message: questionMap.get(cpt) });
 
         }
         console.log('Message send  :',
-            {message: questionMap.get(cpt)},
+            { message: questionMap.get(cpt) },
             ' cpt = ', cpt);
 
-        if( cpt == 1){
+        if (cpt == 1) {
             dataMap.set('birthname', message);
         }
 
-        if( cpt == 2){
+        if (cpt == 2) {
             dataMap.set('lastname', message);
         }
 
-        if( cpt == 3) {
+        if (cpt == 3) {
             dataMap.set('ssn', message);
-            console.log(dataMap);
             asyncCall();
             cpt = 0;
-           }
+        }
 
 
 
@@ -80,7 +79,6 @@ function asyncCall() {
     };
 
 
-    console.log('mon req body', postData);
 
     const clientServerOptions = {
         uri: 'http://localhost:3011/people/',
@@ -94,30 +92,28 @@ function asyncCall() {
     };
 
     request(clientServerOptions, function (error, response, body) {
-           if(error != null){
-               console.log('error:', error);
-           }
-          else{
-               serverResponse = body;
-               console.log('statusCode:',
-                   response && response.statusCode,
-                   'BODY ', serverResponse);
-               if(response.statusCode != 200){
-                   io.sockets.emit('new message',
-                       {message: serverResponse});
-               }else{
-                   io.sockets.emit('new message',
-                       {message: JSON.stringify(serverResponse)});
-               }
+        if (error != null) {
+            console.log('error:', error);
+        }
+        else {
 
-           }
+            serverResponse = body;
+            console.log('statusCode:',
+                response && response.statusCode, 'BODY ', serverResponse);
 
-       });
+            io.sockets.emit('new message', {
+                message:
+                    JSON.stringify(serverResponse)
+            });
+
+
+        }
+
+    });
 
 
 
 }
-
 
 
 app.get('/', (req, res) => {
