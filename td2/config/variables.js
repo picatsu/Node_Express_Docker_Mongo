67853b8bf7e7af875dc32af727d46774785a809c
achraf <achraf.pica@gmail.com>
2020-02-01 00:05:24 +0100

    hopefully, the last commit

--- td2/config/variables.js
+++ td2/config/variables.js
@@ -1,8 +1,8 @@
 module.exports = {
   PORT: 3000,
   URLTD2: 'https://geo.api.gouv.fr',
-  urlMongo: 'localhost',
-  urlApiTd2: 'localhost',
-  urlApiTd3: 'localhost',
+  urlMongo: 'db',
+  urlApiTd2: 'td2',
+  urlApiTd3: 'td3',
   urlLocalhost: 'localhost'
 };
